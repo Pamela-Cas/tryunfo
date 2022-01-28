d72c240183487a56fd9c5f6fd605d696080ff375
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-01-28 16:33:23 -0300

    Requesito 01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Form from './components/form';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Tryunfo</h1>
+        <h1 className="titulo">Tryunfo</h1>
+        <Form />
+
       </div>
     );
   }
