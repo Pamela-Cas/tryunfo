c35028883d5e959861d5ea4491390584c0b226aa
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-01-31 17:30:15 -0300

    requesito 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1 className="titulo">Tryunfo</h1>
+      <>
+        <h1>Tryunfo</h1>
         <Form />
-
-      </div>
+        <Card />
+      </>
     );
   }
 }
 
 export default App;
-// vqv
